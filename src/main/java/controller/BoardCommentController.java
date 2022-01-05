9eb8e59ea57b2e2a63a1e92944679aec1cee4445
cjw <cjw.git@gmail.com>
2022-01-05 20:20:03 +0900

    url update

--- src/main/java/controller/BoardCommentController.java
+++ src/main/java/controller/BoardCommentController.java
@@ -22,43 +22,43 @@ public class BoardCommentController {
     @Autowired
     private IBoardCommentService boardCommentService;
 
-    @RequestMapping(value = "/create-board-comment", method = RequestMethod.POST)
+    @RequestMapping(value = "/board/comment", method = RequestMethod.POST)
     @ApiOperation(value = "댓글 생성", notes = "게시글에 댓글을 생성하기 위한 API입니다. {게시글 고유번호, 내용}")
     public ResponseEntity createComment(@RequestBody @Validated(BoardCommentVO.class) BoardCommentVO boardComment) throws Exception {
         return new ResponseEntity(boardCommentService.createComment(boardComment), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/read-board-comment", method = RequestMethod.GET)
+    @RequestMapping(value = "/board/comment", method = RequestMethod.GET)
     @ApiOperation(value = "댓글 불러오기", notes = "게시글에 댓글을 불러오기 위한 API입니다. {게시글 고유번호}")
     public ResponseEntity getComment(Long board_uid, @RequestParam(required = false, defaultValue = "1") int page, @RequestParam(required = false, defaultValue = "10") int range) throws Exception {
         return new ResponseEntity(boardCommentService.getComment(board_uid, page, range), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/update-board-comment", method = RequestMethod.PUT)
+    @RequestMapping(value = "/board/comment", method = RequestMethod.PUT)
     @ApiOperation(value = "댓글 수정", notes = "게시글에 댓글을 수정하기 위한 API입니다. {게시글 고유번호, 내용}")
     public ResponseEntity updateComment(@RequestBody @Validated(BoardCommentVO.class) BoardCommentVO boardComment) throws Exception {
         return new ResponseEntity(boardCommentService.updateComment(boardComment), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/delete-board-comment", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/board/comment", method = RequestMethod.DELETE)
     @ApiOperation(value = "댓글 삭제", notes = "게시글에 댓글을 삭제하기 위한 API입니다. {게시글 고유번호}")
     public ResponseEntity deleteComment(Long board_comment_uid) throws Exception {
         return new ResponseEntity(boardCommentService.deleteComment(board_comment_uid), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/create-board-comment-emotion", method = RequestMethod.POST)
+    @RequestMapping(value = "/board/comment/emotion", method = RequestMethod.POST)
     @ApiOperation(value = "댓글 감정표현", notes = "댓글에 감정표현을 달기 위한 API입니다. {댓글 고유번호, 공감상태}")
     public ResponseEntity createBoardCommentEmotion(@RequestBody Long board_comment_uid, Integer status) throws Exception {
         return new ResponseEntity(boardCommentService.createBoardCommentEmotion(board_comment_uid, status), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/delete-board-comment-emotion", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/board/comment/emotion", method = RequestMethod.DELETE)
     @ApiOperation(value = "댓글 감정표현 취소", notes = "댓글에 감정표현을 취소 위한 API입니다. {댓글 고유번호}")
     public ResponseEntity deleteBoardCommentEmotion(@RequestBody Long board_comment_uid) throws Exception {
         return new ResponseEntity(boardCommentService.deleteBoardCommentEmotion(board_comment_uid), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/read-board-comment-emotion", method = RequestMethod.GET)
+    @RequestMapping(value = "/board/comment/emotion", method = RequestMethod.GET)
     @ApiOperation(value = "댓글 감정표현 개수", notes = "댓글에 감정표현의 개수를 보기 위한 API입니다. {댓글 고유번호}")
     public ResponseEntity getBoardCommentEmotion(Long board_comment_uid) throws Exception {
         return new ResponseEntity(boardCommentService.getBoardCommentEmotion(board_comment_uid), HttpStatus.OK);
